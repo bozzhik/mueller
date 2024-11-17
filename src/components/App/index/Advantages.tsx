1dc95fa5fc1622e8ab9db4a6fbd9a9d59158705d
maxim <bozzhik@ya.ru>
2024-11-17 17:22:54 +0300

    finalize `Advantages` index block

--- src/components/App/index/Advantages.tsx
+++ src/components/App/index/Advantages.tsx
@@ -1,7 +1,10 @@
 import AdvantagesImage from '$/index/advantages.jpg'
+import DepositIcon from '$/index/success/deposit.svg'
+import ChessPiecesIcon from '$/index/success/chess-pieces.svg'
+import ChessBoardIcon from '$/index/success/chess-board.svg'
 
 import Image from 'next/image'
-import {H2, H6, P} from '~/UI/Typography'
+import {H2, H3, H4, H6, P} from '~/UI/Typography'
 
 const advantagesData = {
   1: 'Возможность проведения встреч в России, Германии и на Кипре.',
@@ -10,21 +13,69 @@ const advantagesData = {
   4: 'Независимость от санкционной политики и политической конъюнктуры международных юридических компаний.',
 }
 
+const specializationData = {
+  1: {
+    heading: 'Банки',
+    content: ['Поиск и разблокировка активов в европейских банках', 'Предъявление банковских гарантий'],
+    image: DepositIcon,
+  },
+  2: {
+    heading: 'Ценные бумаги',
+    content: ['Разблокировка ценных бумаг у брокеров, депозитариев в ЕС, США и Швейцарии', 'Перевод ценных бумаг в Россию из зарубежных юрисдикций'],
+    image: ChessPiecesIcon,
+  },
+  3: {
+    heading: 'Международные сделки',
+    content: ['Возврат предоплаты', 'Получение разрешений национальных регуляторов', 'Принуждение зарубежных контрагентов к выполнению договоров.'],
+    image: ChessBoardIcon,
+  },
+}
+
 export default function Advantages() {
   return (
     <section data-section="advantages-index" className="relative grid grid-cols-2">
       <div>
-        <div className="py-10 text-white px-7 bg-blue">
-          <H2>НАШИ ПРЕИМУЩЕСТВА</H2>
+        <div>
+          <div className="py-10 text-white px-7 bg-blue">
+            <H2>НАШИ ПРЕИМУЩЕСТВА</H2>
+          </div>
+
+          <div className="divide-y divide-gray">
+            {Object.entries(advantagesData).map(([key, value]) => (
+              <div key={key} className="flex px-10 gap-7 py-14">
+                <H6 className="text-3xl leading-none">{key}</H6>
+                <P className="uppercase">{value}</P>
+              </div>
+            ))}
+          </div>
         </div>
 
-        <div className="divide-y divide-gray">
-          {Object.entries(advantagesData).map(([key, value]) => (
-            <div key={key} className="flex px-10 gap-7 py-14">
-              <H6 className="text-3xl leading-none">{key}</H6>
-              <P className="uppercase">{value}</P>
-            </div>
-          ))}
+        <div>
+          <div className="py-10 text-white px-7 bg-blue">
+            <H2>НАША СПЕЦИАЛИЗАЦИЯ</H2>
+          </div>
+
+          <div className="divide-y divide-gray">
+            {Object.entries(specializationData).map(([key, {heading, content, image}]) => (
+              <div key={key} className="flex items-start gap-10 px-10 py-14">
+                <div>
+                  <Image className="block object-contain w-full h-full" src={image} alt={heading} />
+                </div>
+
+                <div className="space-y-0">
+                  <H3>{heading}</H3>
+
+                  <div className="space-y-4 divide-y divide-gray">
+                    {content.map((item, index) => (
+                      <H4 className="uppercase max-w-[40ch] pt-4" key={index}>
+                        {item}
+                      </H4>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
 
--- src/components/UI/Typography.tsx
+++ src/components/UI/Typography.tsx
@@ -6,11 +6,14 @@ type Props = {
   children: React.ReactNode
 }
 
-type TypoTypes = 'h1' | 'h2' | 'h6' | 'p'
+type TypoTypes = 'h1' | 'h2' | 'h3' | 'h4' | 'h6' | 'p'
 
 export const typoClasses = {
-  h1: 'text-8xl xl:text-[66px] sm:text-4xl uppercase font-kaius font-bold', // H1 96px
-  h2: 'text-[44px] uppercase font-kaius font-bold', // H2 44px
+  h1: 'text-8xl xl:text-[66px] sm:text-4xl uppercase font-bold font-kaius', // H1 96px
+  h2: 'text-[44px] uppercase font-bold font-kaius', // H2 44px
+  h3: 'text-[28px] uppercase font-bold font-kaius', // H3 28px
+
+  h4: 'text-2xl font-playfair', // H3 24px
   h6: 'text-2xl xl:text-xl sm:text-lg font-light font-playfair', // Subscript[1] 24px
   p: 'text-3xl xl:text-2xl sm:text-xl font-light font-playfair', // Paragraph 28px
 }
@@ -32,5 +35,7 @@ const initElement = (type: TypoTypes) =>
 
 export const H1 = initElement('h1')
 export const H2 = initElement('h2')
+export const H3 = initElement('h3')
+export const H4 = initElement('h4')
 export const H6 = initElement('h6')
 export const P = initElement('p')

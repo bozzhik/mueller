aa053d8ecdcfe5e11a2bacd38de6665b35326ec2
maxim <bozzhik@ya.ru>
2024-11-18 21:11:48 +0300

    [2] fix `failed` vercel build
    Type error: Type '{ data: DETAILS_QUERYResult; sourceMap: ContentSourceMap | null; tags: string[]; }' is not assignable to type '{ data: Detail[]; }'.

--- src/components/App/index/Details/Details.tsx
+++ src/components/App/index/Details/Details.tsx
@@ -1,16 +1,15 @@
-import {Detail} from '#/sanity.types'
 import {sanityFetch} from '@/sanity/lib/live'
 import {DETAILS_QUERY} from '@/sanity/lib/queries'
 
 import DetailsModule from '~~/index/Details/DetailsModule'
 
 export default async function Details() {
-  const {data: details}: {data: Detail[]} = await sanityFetch({
+  const {data: details} = await sanityFetch({
     query: DETAILS_QUERY,
   })
 
-  const advantagesData = details.find((item: Detail) => item.name === 'Преимущества')?.advantages || []
-  const specializationData = details.find((item: Detail) => item.name === 'Специализация')?.specialization || []
+  const advantagesData = details.find((item) => item.name === 'Преимущества')?.advantages || []
+  const specializationData = details.find((item) => item.name === 'Специализация')?.specialization || []
 
   return <DetailsModule advantages={advantagesData} specialization={specializationData} />
 }

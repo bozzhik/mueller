2df1821379f1129913c45956352a0f2b966f92fd
maxim <bozzhik@ya.ru>
2024-11-20 16:04:12 +0300

    init `getPublisher` inside of `NewsCard` sub-component

--- src/components/App/index/News/NewsCard.tsx
+++ src/components/App/index/News/NewsCard.tsx
@@ -19,12 +19,34 @@ export function NewsCard({heading, caption, publisher, source, id, image, media,
   const imageUrl = cms ? (image?.asset ? urlFor(image).url() : null) : media?.url || null
   const imageAlt = cms ? image?.alt : media?.alt
 
+  const DOMAIN_NAME_MAP: Record<string, string> = {
+    'rbc.ru': 'РБК',
+    'kommersant.ru': 'Коммерсант',
+  }
+
+  function getPublisher(source?: string | null, publisher?: string | null): string {
+    if (publisher) return publisher
+
+    if (source) {
+      function getBaseDomain(hostname: string): string {
+        const parts = hostname.split('.')
+        return parts.slice(-2).join('.')
+      }
+
+      const hostname = new URL(source).hostname.replace('www.', '')
+      const baseDomain = getBaseDomain(hostname)
+      return DOMAIN_NAME_MAP[baseDomain] || hostname
+    }
+
+    return ''
+  }
+
   return (
     <Link href={source || ''} className={`${id == 4 && 'xl:hidden'}`}>
-      {imageUrl && <Image quality={100} className="border-b border-gray block w-full h-[40vh] xl:h-[35vh] object-cover" src={imageUrl} alt={imageAlt || 'Новость про Mueller Wagner'} width={300} height={200} />}
+      {imageUrl && <Image quality={100} className="border-b border-t border-gray block w-full h-[40vh] xl:h-[35vh] object-cover" src={imageUrl} alt={imageAlt || 'Новость про Mueller Wagner'} width={300} height={200} />}
 
       <div className="px-6 pt-4 pb-16 xl:px-4 xl:pt-3 xl:pb-14 sm:pt-5 sm:pb-7 space-y-5 xl:space-y-2">
-        <H6>{publisher}</H6>
+        <H6 className="uppercase">{getPublisher(source, publisher)}</H6>
 
         <div className="space-y-2 xl:space-y-1.5">
           <H4 className="leading-[1.2] xl:leading-[1.2] font-bold uppercase font-kaius">{heading}</H4>
--- src/components/App/index/News/NewsGrid.tsx
+++ src/components/App/index/News/NewsGrid.tsx
@@ -9,10 +9,10 @@ type NewsGridItem = {
 } & NEWS_QUERYResult[number]
 
 export default function NewsGrid({news, cms}: {news: NewsGridItem[]; cms: boolean}) {
-  const displayedNews = cms ? news.sort((a, b) => (a.id ?? 0) - (b.id ?? 0)).slice(0, 4) : news.slice(0, 4)
+  const displayedNews = cms ? news.sort((a, b) => (a.id ?? 0) - (b.id ?? 0)).slice(0, 4) : news
 
   return (
-    <section data-section="news-grid" className="grid grid-cols-4 xl:grid-cols-3 sm:grid-cols-1 divide-x divide-gray border-t border-b border-gray">
+    <section data-section="news-grid" className="grid grid-cols-4 xl:grid-cols-3 sm:grid-cols-1 divide-x divide-gray">
       {displayedNews.map((item, index) => (
         <NewsCard key={index} cms={cms} {...item} />
       ))}

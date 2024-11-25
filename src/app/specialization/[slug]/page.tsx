bd85d85b85fb9ad849f77d04877ff982341dada4
maxim <bozzhik@ya.ru>
2024-11-25 20:13:27 +0300

    remodel `Hero` specialization block

--- src/app/specialization/[slug]/page.tsx
+++ src/app/specialization/[slug]/page.tsx
@@ -9,7 +9,6 @@ import News from '~~/index/News/News'
 import Contacts from '~~/index/Contacts'
 
 import Hero from '~~/specialization/Hero'
-import {P} from '~/UI/Typography'
 
 export default async function Page({params}: {params: Promise<QueryParams>}) {
   const {data: specialization} = await sanityFetch({
@@ -21,21 +20,12 @@ export default async function Page({params}: {params: Promise<QueryParams>}) {
     return notFound()
   }
 
-  const {heading, list, icon, image} = specialization
+  const {heading, list, image} = specialization
 
   return (
     <Container>
       {/* @ts-expect-error i think its because sanity new version has some bugs */}
-      <Hero heading={heading} icon={icon} image={image} />
-
-      <div className={`grid grid-cols-2 sm:grid-cols-1 sm:pb-1 divide-x sm:divide-y divide-gray border-t border-b border-gray ${list?.length && list.length > 2 && 'divide-y'}`}>
-        {list?.map((item, idx) => (
-          <div className="flex gap-8 sm:gap-6 items-center px-7 sm:px-4 pt-0 pb-4" key={idx}>
-            <div className="text-[70px] xl:text-6xl s-fit leading-normal">{`${idx + 1}`}</div>
-            <P className="pt-4">{`${item}`}</P>
-          </div>
-        ))}
-      </div>
+      <Hero heading={heading} list={list} image={image} />
 
       <News />
       <Contacts />
--- src/components/App/specialization/Hero.tsx
+++ src/components/App/specialization/Hero.tsx
@@ -4,17 +4,16 @@ import {cn} from '#/src/lib/utils'
 import {urlFor} from '#/src/sanity/lib/image'
 
 import Image from 'next/image'
-import {H1} from '~/UI/Typography'
+import {H1, P} from '~/UI/Typography'
 
 type Props = {
   heading: string | null
-  icon: never
+  list: string[] | null
   image: never
 }
 
-export default function Hero({heading, icon, image}: Props) {
+export default function Hero({heading, list, image}: Props) {
   const imageUrl = image ? urlFor(image).url() : null
-  const iconUrl = icon ? urlFor(icon).url() : null
 
   return (
     <section data-section="hero-specialization" className={cn('grid grid-cols-2 sm:flex sm:flex-col-reverse', blockHeight, 'sm:h-auto sm:gap-5 sm:mt-10')}>
@@ -24,14 +23,17 @@ export default function Hero({heading, icon, image}: Props) {
         </div>
       )}
 
-      <div className="flex flex-col justify-between p-7 sm:gap-5 sm:p-3">
-        {iconUrl && (
-          <div className="s-32 xl:s-20 sm:s-16">
-            <Image quality={100} className="block object-cover w-full h-full" src={iconUrl} alt={heading || ''} width={1000} height={1000} />
-          </div>
-        )}
+      <div className="flex flex-col justify-between  sm:gap-5 ">
+        <H1 className="p-7 sm:p-3">{heading || 'Default Heading'}</H1>
 
-        <H1 className="">{heading || 'Default Heading'}</H1>
+        <div className={`flex flex-col sm:pb-1 divide-y divide-gray border-t border-b border-gray ${list?.length && list.length > 2 && 'divide-y'}`}>
+          {list?.map((item, idx) => (
+            <div className="p-7 sm:p-3 flex gap-8 sm:gap-6 items-center sm:px-4 pt-0 pb-4" key={idx}>
+              <div className="text-[70px] xl:text-6xl s-fit leading-normal">{`${idx + 1}`}</div>
+              <P className="pt-4">{`${item}`}</P>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )

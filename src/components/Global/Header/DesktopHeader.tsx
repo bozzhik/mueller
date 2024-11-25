0ae3aa7737e208df603b82adcab26690948e5a1f
maxim <bozzhik@ya.ru>
2024-11-25 21:47:01 +0300

    destruct `Header` Global component

---
+++ src/components/Global/Header/DesktopHeader.tsx
@@ -0,0 +1,31 @@
+import MuellerLogo from '$/logo.svg'
+
+import {websitePaths} from '#/src/lib/constants'
+import {hoverLinkStyles} from '~/Global/Footer'
+
+import Link from 'next/link'
+import Image from 'next/image'
+import {SPAN} from '~/UI/Typography'
+
+export function DesktopHeader() {
+  return (
+    <header className="sm:hidden fixed z-[99] grid w-full h-[9vh] grid-cols-10 bg-white border-b border-gray">
+      {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+      <a href="/" className="grid col-span-2 border-r place-items-center border-gray">
+        <Image src={MuellerLogo} className="object-contain w-[85%]" alt="Mueller Wagner logo" />
+      </a>
+
+      <nav className="flex items-center justify-center col-span-6 gap-8 xl:gap-4 -mt-1.5 xl:-mt-0.5">
+        {Object.entries(websitePaths).map(([key, label]) => (
+          <Link href={`/#${key}`} className={hoverLinkStyles} key={key}>
+            <SPAN>{label}</SPAN>
+          </Link>
+        ))}
+      </nav>
+
+      <Link href="presentation.pdf" target="_blank" className="grid col-span-2 text-white bg-blue duration-500 hover:bg-blue/95 place-items-center -mt-1.5 xl:-mt-0.5">
+        <SPAN className="pb-1 text-2xl normal-case xl:text-xl">Скачать презентацию</SPAN>
+      </Link>
+    </header>
+  )
+}

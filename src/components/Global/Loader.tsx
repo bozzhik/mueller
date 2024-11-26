aecd9f764ad433978f20780a5a25cc343dde22b1
maxim <bozzhik@ya.ru>
2024-11-26 21:04:17 +0300

    adapt `Loader` Global component

--- src/components/Global/Loader.tsx
+++ src/components/Global/Loader.tsx
@@ -1,11 +1,15 @@
 'use client'
 
 import {useEffect, useState} from 'react'
+import {useMediaQuery} from '@/hooks/useMediaQuery'
+
 import {H4} from '~/UI/Typography'
 
 export default function Loader() {
   const [isVisible, setIsVisible] = useState(true)
+  const [isFading, setIsFading] = useState(false) // Track fading animation
   const [showSkipButton, setShowSkipButton] = useState(false)
+  const isDesktop = useMediaQuery('(min-width: 768px)')
 
   useEffect(() => {
     if (isVisible) {
@@ -27,15 +31,22 @@ export default function Loader() {
     return () => clearTimeout(timer)
   }, [])
 
+  const handleSkip = () => {
+    setIsFading(true)
+    setTimeout(() => {
+      setIsVisible(false)
+    }, 315)
+  }
+
   return (
     isVisible && (
-      <section data-section="loader" className="fixed grid place-items-center w-screen h-screen bg-[#000] z-[99999] overflow-hidden">
+      <section data-section="loader" className={`fixed grid place-items-center w-screen h-screen bg-[#000] z-[99999] overflow-hidden duration-300 ${isFading ? 'opacity-0' : 'opacity-100'}`}>
         <div className="w-full h-full">
-          <video autoPlay loop muted playsInline className="block object-cover w-full h-full bg-gray-light/25" src="/loader/desktop.webm" />
+          <video autoPlay loop muted playsInline className="block object-cover w-full h-full bg-gray-light/25" src={isDesktop ? '/loader/desktop.webm' : '/loader/mobile.webm'} />
         </div>
 
         {showSkipButton && (
-          <button className="absolute px-10 pt-3 pb-4 text-white duration-200 rounded-md bottom-7 bg-blue/70 hover:bg-blue" onClick={() => setIsVisible(false)}>
+          <button className="absolute px-10 pt-3 pb-4 text-white duration-200 rounded-md sm:py-3 sm:px-7 bottom-7 bg-blue/85 hover:bg-blue" onClick={handleSkip}>
             <H4 className="sm:text-lg">Пропустить видео</H4>
           </button>
         )}

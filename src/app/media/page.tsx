008aa889653193d76e73c2d51ed00151cc7f60b3
maxim <bozzhik@ya.ru>
2024-11-26 17:28:53 +0300

    remodel `/media` page

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,39 +1,19 @@
-import axios from 'axios'
+import {sanityFetch} from '@/sanity/lib/live'
+import {NEWS_QUERY} from '@/sanity/lib/queries'
 
 import Container from '~/Global/Container'
 import RunningLine from '~~/news/RunningLine'
 import NewsGrid from '~~/index/News/NewsGrid'
 
-type TFeedItem = {
-  url: string
-  title: string
-  content_text: string
-  image: {
-    src: string
-    alt: string
-  }
-}
-
 export default async function MediaPage() {
-  const rssFeedUrl = process.env.NEXT_PUBLIC_RSS_FEED_URL || 'https://rss.app/feeds/v1.1/5cDiZJxRvi4fcr5v.json'
-  const {data} = await axios.get(rssFeedUrl)
-
-  const news = data?.items.map((item: TFeedItem) => ({
-    heading: item.title,
-    caption: item.content_text,
-    source: item.url,
-    media: item.image
-      ? {
-          url: item.image,
-          alt: item.title,
-        }
-      : null,
-  }))
+  const {data} = await sanityFetch({
+    query: NEWS_QUERY,
+  })
 
   return (
     <Container>
       <RunningLine />
-      <NewsGrid news={news} cms={false} />
+      <NewsGrid news={data} isIndex={false} />
     </Container>
   )
 }

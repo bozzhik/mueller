241a7d4c206379d003834cab6249ab8857fa1642
maxim <bozzhik@ya.ru>
2024-11-27 00:42:03 +0300

    remodel `DesktopHeader` Global sub-component

--- src/components/Global/Header/DesktopHeader.tsx
+++ src/components/Global/Header/DesktopHeader.tsx
@@ -1,12 +1,10 @@
 'use client'
 
 import MuellerLogo from '$/logo.svg'
-
 import {PRESENTATIONS_QUERYResult} from '#/sanity.types'
+
 import {websitePaths} from '#/src/lib/constants'
 import {hoverLinkStyles} from '~/Global/Footer'
-
-import {usePathname} from 'next/navigation'
 import {urlForFile} from '#/src/sanity/lib/file'
 
 import Link from 'next/link'
@@ -16,35 +14,44 @@ import {SPAN} from '~/UI/Typography'
 type PRESENTATIONS_QUERYResultItem = PRESENTATIONS_QUERYResult[number]
 
 export function DesktopHeader({action}: {action: PRESENTATIONS_QUERYResultItem[]}) {
-  const pathname = usePathname()
+  const generalPresentation = action.find((p: PRESENTATIONS_QUERYResultItem) => p.name === 'Общая')
+  const securitiesPresentation = action.find((p: PRESENTATIONS_QUERYResultItem) => p.name === 'Ценные бумаги')
 
-  const [defaultPresentation] = action.filter((p: PRESENTATIONS_QUERYResultItem) => p.name === 'Общая')
-  const currentPresentation = action.find((p: PRESENTATIONS_QUERYResultItem) => pathname.includes('czennye-bumagi') && p.name === 'Ценные бумаги') || defaultPresentation
+  if (!generalPresentation || !securitiesPresentation) return null
 
-  if (!currentPresentation) return null
+  const presentations = [
+    {presentation: generalPresentation, name: 'Общая'},
+    {presentation: securitiesPresentation, name: 'Ценные бумаги'},
+  ]
 
   return (
-    <header className="sm:hidden fixed z-[99] grid w-full h-[9vh] grid-cols-10 bg-white border-b border-gray">
-      <Link href="/" className="grid col-span-2 border-r place-items-center border-gray">
-        <Image src={MuellerLogo} className="object-contain w-[85%]" alt="Mueller Wagner logo" />
-      </Link>
+    <header className="sm:hidden fixed z-[99] w-full h-[9vh] flex justify-between bg-white border-b border-gray">
+      <div className="flex gap-10 xl:gap-6">
+        <Link href="/" className="grid px-8 xl:px-4 border-r place-items-center border-gray xl:w-[30%]">
+          <Image src={MuellerLogo} className="object-contain" alt="Mueller Wagner logo" />
+        </Link>
 
-      <nav className="flex items-center justify-center col-span-6 gap-8 xl:gap-4 -mt-1.5 xl:-mt-0.5">
-        {Object.entries(websitePaths).map(([key, label]) => {
-          const isMedia = key === 'media'
-          const href = isMedia ? '/media' : `/#${key}`
+        <nav className="flex items-center justify-center gap-8 xl:gap-4 -mt-1.5 xl:-mt-0.5">
+          {Object.entries(websitePaths).map(([key, label]) => {
+            const isMedia = key === 'media'
+            const href = isMedia ? '/media' : `/#${key}`
 
-          return (
-            <Link href={href} className={hoverLinkStyles} key={key}>
-              <SPAN>{label}</SPAN>
-            </Link>
-          )
-        })}
-      </nav>
+            return (
+              <Link href={href} className={hoverLinkStyles} key={key}>
+                <SPAN>{label}</SPAN>
+              </Link>
+            )
+          })}
+        </nav>
+      </div>
 
-      <Link href={urlForFile(currentPresentation.file?.asset?._ref || '')} target="_blank" className="grid col-span-2 text-white bg-blue duration-500 hover:bg-blue/95 place-items-center -mt-1.5 xl:-mt-0.5">
-        <SPAN className="pb-1 text-2xl normal-case xl:text-xl">{currentPresentation.caption}</SPAN>
-      </Link>
+      <div className="flex divide-x-[3px] divide-gray-light">
+        {presentations.map(({presentation, name}) => (
+          <Link key={name} href={urlForFile(presentation?.file?.asset?._ref || '')} target="_blank" className="grid px-10 xl:px-7 text-white bg-blue duration-500 hover:bg-blue/95 place-items-center -mt-1.5 xl:-mt-0.5">
+            <SPAN className="block pb-1 text-2xl normal-case text-nowrap">{presentation?.caption}</SPAN>
+          </Link>
+        ))}
+      </div>
     </header>
   )
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,7 +2,6 @@ export const websitePaths = {
   advantages: 'Преимущества',
   specialization: 'Специализация',
   achievements: 'Успехи',
-  blog: 'Блог',
   media: 'Мы в СМИ',
   contacts: 'Контакты',
 }

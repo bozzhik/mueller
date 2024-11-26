8b5032e9fb0b90f0a724079bb64f79e6ee78cc70
maxim <bozzhik@ya.ru>
2024-11-26 18:51:03 +0300

    remove `infinityScroll` functionality from `NewsGrid`

--- src/components/App/specialization/NewsGrid.tsx
+++ src/components/App/specialization/NewsGrid.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import {NEWS_QUERYResult} from '#/sanity.types'
-
-import {useState, useEffect, useCallback} from 'react'
+import {useMediaQuery} from '#/src/hooks/useMediaQuery'
 import {NewsCard} from '~~/index/News/NewsCard'
 
 type NewsGridItem = {
@@ -13,33 +12,9 @@ type NewsGridItem = {
 } & NEWS_QUERYResult[number]
 
 export default function NewsGrid({news}: {news: NewsGridItem[]}) {
-  const [displayedNews, setDisplayedNews] = useState<NewsGridItem[]>([])
-  const [itemsToLoad, setItemsToLoad] = useState(8)
+  const isDesktop = useMediaQuery('(min-width: 768px)')
 
-  const handleScroll = useCallback(() => {
-    const buffer = 800
-    const bottomReached = window.innerHeight + window.scrollY + buffer >= document.body.offsetHeight
-
-    if (bottomReached) {
-      setItemsToLoad((prev) => prev + 8)
-    }
-  }, [])
-
-  useEffect(() => {
-    setDisplayedNews(news.slice(0, itemsToLoad))
-  }, [itemsToLoad, news])
-
-  useEffect(() => {
-    const optimizedHandleScroll = () => {
-      requestAnimationFrame(handleScroll)
-    }
-
-    window.addEventListener('scroll', optimizedHandleScroll)
-
-    return () => {
-      window.removeEventListener('scroll', optimizedHandleScroll)
-    }
-  }, [handleScroll])
+  const displayedNews = isDesktop ? news.slice(0, 12) : news.slice(0, 4)
 
   return (
     <section data-section="news-grid" className="grid grid-cols-4 border-t xl:grid-cols-3 sm:grid-cols-1 border-gray">

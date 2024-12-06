c4ec361f40785f491a42cb47ce8e1d6e2931cae9
maxim <bozzhik@ya.ru>
2024-12-06 23:48:19 +0300

    init `/euroclear` page

---
+++ src/app/euroclear/page.tsx
@@ -0,0 +1,14 @@
+import {H1} from '~/UI/Typography'
+
+import Container from '~/Global/Container'
+import Contacts from '~~/specialization/Contacts'
+
+export default function EuroclearPage() {
+  return (
+    <Container>
+      <H1>Euroclear</H1>
+
+      <Contacts />
+    </Container>
+  )
+}

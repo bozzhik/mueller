06d920931106821ee2485bfacbc89ef8ad7e4e2c
maxim <bozzhik@ya.ru>
2024-12-07 18:02:42 +0300

    integrate `HoverText` into `Specialization` index sub-block

--- src/components/App/index/Details/Specialization.tsx
+++ src/components/App/index/Details/Specialization.tsx
@@ -1,11 +1,18 @@
+'use client'
+
 import {SpecializationItem} from '#/sanity.types'
 import {urlFor} from '#/src/sanity/lib/image'
+
+import {useRef} from 'react'
 
 import Link from 'next/link'
 import Image from 'next/image'
 import {H2, H3, H4} from '~/UI/Typography'
+import HoverText from '~/UI/HoverText'
 
 export default function Specialization({data}: {data: SpecializationItem[]}) {
+  const itemRefs = useRef<(HTMLAnchorElement | null)[]>([])
+
   return (
     <section id="specialization" data-section="details-specialization-index">
       <div className="p-10 text-white sm:px-5 sm:py-4 xl:py-8 bg-blue">
@@ -17,7 +24,14 @@ export default function Specialization({data}: {data: SpecializationItem[]}) {
           const imageUrl = icon?.asset ? urlFor(icon).url() : null
 
           return (
-            <Link href={`/specialization/${slug}`} className="flex items-start gap-10 px-10 group sm:gap-7 sm:pl-3 sm:pr-6 sm:flex-col py-14 xl:py-10 sm:py-7" key={idx}>
+            <Link
+              ref={(el) => {
+                itemRefs.current[idx] = el
+              }}
+              href={`/specialization/${slug}`}
+              className="flex items-start gap-10 px-10 group sm:gap-7 sm:pl-3 sm:pr-6 sm:flex-col py-14 xl:py-10 sm:py-7"
+              key={idx}
+            >
               {imageUrl && (
                 <div className="s-32 xl:s-20 sm:s-16 group-hover:scale-[1.07] duration-500">
                   <Image className="block object-contain w-full h-full" src={imageUrl} alt={heading || ''} width={128} height={128} />
@@ -25,7 +39,9 @@ export default function Specialization({data}: {data: SpecializationItem[]}) {
               )}
 
               <div className="sm:space-y-1">
-                <H3>{heading}</H3>
+                <HoverText triggerRef={{current: itemRefs.current[idx]}}>
+                  <H3>{heading}</H3>
+                </HoverText>
 
                 <div className="space-y-4 divide-y sm:space-y-3 divide-gray sm:divide-gray/30">
                   {list?.map((item, index) => (

157d3df8c0ea149cf2eaf56ff35bee9b4dfafe74
maxim <bozzhik@ya.ru>
2024-12-08 16:39:53 +0300

    refactor `ImageShader` UI component

--- src/components/UI/ImageShader.tsx
+++ src/components/UI/ImageShader.tsx
@@ -15,17 +15,24 @@ type Props = {
 export default function ImageShader({src, alt = '', className}: Props) {
   const containerRef = useRef<HTMLDivElement>(null)
 
+  const isMobileDevice = () => {
+    return typeof window !== 'undefined' && window.matchMedia('(max-width: 990px)').matches
+  }
+
   useEffect(() => {
+    if (isMobileDevice()) return
+
     let scene: THREE.Scene
-    let camera: THREE.OrthographicCamera
+    let camera: THREE.PerspectiveCamera
     let renderer: THREE.WebGLRenderer
     let planeMesh: THREE.Mesh<THREE.PlaneGeometry, THREE.ShaderMaterial>
     let animationFrameId: number
 
-    const easeFactor = 0.02
+    const easeFactor = {current: 0.02}
     const mousePosition = {x: 0.5, y: 0.5}
     const targetMousePosition = {x: 0.5, y: 0.5}
     const prevPosition = {x: 0.5, y: 0.5}
+    let aberrationIntensity = 0.0
 
     const vertexShader = `
       varying vec2 vUv;
@@ -40,11 +47,11 @@ export default function ImageShader({src, alt = '', className}: Props) {
       uniform sampler2D u_texture;
       uniform vec2 u_mouse;
       uniform vec2 u_prevMouse;
-      uniform vec2 u_gridSize;
+      uniform float u_aberrationIntensity;
 
       void main() {
-        vec2 gridUV = floor(vUv * u_gridSize) / u_gridSize;
-        vec2 centerOfPixel = gridUV + vec2(1.0 / u_gridSize.x, 1.0 / u_gridSize.y);
+        vec2 gridUV = floor(vUv * vec2(20.0, 20.0)) / vec2(20.0, 20.0);
+        vec2 centerOfPixel = gridUV + vec2(1.0 / 20.0, 1.0 / 20.0);
 
         vec2 mouseDirection = u_mouse - u_prevMouse;
 
@@ -52,11 +59,14 @@ export default function ImageShader({src, alt = '', className}: Props) {
         float pixelDistanceToMouse = length(pixelToMouseDirection);
         float strength = smoothstep(0.3, 0.0, pixelDistanceToMouse);
 
-        vec2 uvOffset = strength * -mouseDirection * 0.4;
+        vec2 uvOffset = strength * -mouseDirection * 0.2;
         vec2 uv = vUv - uvOffset;
 
-        vec4 color = texture2D(u_texture, uv);
-        gl_FragColor = color;
+        vec4 colorR = texture2D(u_texture, uv + vec2(strength * u_aberrationIntensity * 0.01, 0.0));
+        vec4 colorG = texture2D(u_texture, uv);
+        vec4 colorB = texture2D(u_texture, uv - vec2(strength * u_aberrationIntensity * 0.01, 0.0));
+
+        gl_FragColor = vec4(colorR.r, colorG.g, colorB.b, 1.0);
       }
     `
 
@@ -88,16 +98,15 @@ export default function ImageShader({src, alt = '', className}: Props) {
       const {clientWidth: width, clientHeight: height} = container
 
       scene = new THREE.Scene()
-      const aspectRatio = width / height
-      camera = new THREE.OrthographicCamera(-1, 1, 1 / aspectRatio, -1 / aspectRatio, 0.1, 1000)
+      camera = new THREE.PerspectiveCamera(80, width / height, 0.01, 10)
       camera.position.z = 1
 
       const texture = createTexture()
       const shaderUniforms = {
         u_mouse: {value: new THREE.Vector2()},
         u_prevMouse: {value: new THREE.Vector2()},
+        u_aberrationIntensity: {value: 0.0},
         u_texture: {value: texture},
-        u_gridSize: {value: new THREE.Vector2(300, 300)},
       }
 
       planeMesh = new THREE.Mesh(
@@ -120,11 +129,14 @@ export default function ImageShader({src, alt = '', className}: Props) {
     const animateScene = () => {
       animationFrameId = requestAnimationFrame(animateScene)
 
-      mousePosition.x += (targetMousePosition.x - mousePosition.x) * easeFactor
-      mousePosition.y += (targetMousePosition.y - mousePosition.y) * easeFactor
+      mousePosition.x += (targetMousePosition.x - mousePosition.x) * easeFactor.current
+      mousePosition.y += (targetMousePosition.y - mousePosition.y) * easeFactor.current
 
       planeMesh.material.uniforms.u_mouse.value.set(mousePosition.x, 1.0 - mousePosition.y)
       planeMesh.material.uniforms.u_prevMouse.value.set(prevPosition.x, 1.0 - prevPosition.y)
+
+      aberrationIntensity = Math.max(0.0, aberrationIntensity - 0.05)
+      planeMesh.material.uniforms.u_aberrationIntensity.value = aberrationIntensity
 
       renderer.render(scene, camera)
     }
@@ -138,22 +150,8 @@ export default function ImageShader({src, alt = '', className}: Props) {
 
       targetMousePosition.x = (event.clientX - rect.left) / rect.width
       targetMousePosition.y = (event.clientY - rect.top) / rect.height
-    }
 
-    const handleResize = () => {
-      const container = containerRef.current
-      if (!container) return
-
-      const {clientWidth: width, clientHeight: height} = container
-
-      const aspectRatio = width / height
-      camera.left = -1
-      camera.right = 1
-      camera.top = 1 / aspectRatio
-      camera.bottom = -1 / aspectRatio
-      camera.updateProjectionMatrix()
-
-      renderer.setSize(width, height)
+      aberrationIntensity = 1.0
     }
 
     const container = containerRef.current
@@ -161,14 +159,17 @@ export default function ImageShader({src, alt = '', className}: Props) {
     animateScene()
 
     container?.addEventListener('mousemove', handleMouseMove)
-    window.addEventListener('resize', handleResize)
 
     return () => {
       cancelAnimationFrame(animationFrameId)
       container?.removeChild(renderer.domElement)
-      window.removeEventListener('resize', handleResize)
     }
   }, [src])
+
+  if (isMobileDevice()) {
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img src={typeof src === 'string' ? src : src.src} alt={alt} className={className} />
+  }
 
   return <div ref={containerRef} className={cn('relative', className)} aria-label={alt} style={{width: '100%', height: '100%'}} />
 }

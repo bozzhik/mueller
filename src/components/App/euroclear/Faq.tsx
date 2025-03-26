fbd032e743d6d27a617a7eb30971ad01ed139455
maxim <bozzhik@ya.ru>
2025-03-26 21:50:44 +0300

    remove `ActionModule` UI component

--- src/components/App/euroclear/Faq.tsx
+++ src/components/App/euroclear/Faq.tsx
@@ -1,13 +1,38 @@
-import ActionModule from '~/UI/ActionModule'
+'use client'
+
+import {useRef} from 'react'
+
+import Link from 'next/link'
+import {H2, H3, H6} from '~/UI/Typography'
+import HoverText from '~/UI/HoverText'
+
+const FaqData = {
+  caption: 'FAQ',
+  title: 'Главные ответы про разблокировку',
+  link: 'https://t.me/sanctionsexplained/226',
+  actionText: 'Ответы В Телеграм',
+  actionButtonText: 'Перейти к публикации',
+}
 
 export default function Faq() {
+  const linkRef = useRef<HTMLAnchorElement>(null)
+
   return (
-    <ActionModule
-      caption="FAQ"
-      title="Главные ответы про разблокировку"
-      link="https://t.me/sanctionsexplained/226"
-      actionText="Ответы В Телеграм" // big square button
-      actionButtonText="Перейти к публикации" // blue button
-    />
+    <section id="blog" data-section="blog-index" className="border-b border-gray">
+      <div className="w-full h-8 sm:h-6 bg-blue"></div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-1 sm:gap-6 items-center p-8 sm:p-3 sm:pb-6">
+        <div className=" space-y-3">
+          <H6>{FaqData.caption}</H6>
+          <H2 className="leading-[1.2]">{FaqData.title}</H2>
+        </div>
+
+        <Link ref={linkRef} href={FaqData.link} className="block h-fit sm:w-full ml-10 sm:m-0 mt-7 pt-5 sm:pt-4 pb-3.5 sm:pb-2 text-center text-white bg-blue">
+          <HoverText triggerRef={linkRef}>
+            <H3 className="text-2xl sm:text-lg mx-auto leading-tight w-fit">{FaqData.actionButtonText}</H3>
+          </HoverText>
+        </Link>
+      </div>
+    </section>
   )
 }
--- src/components/App/index/Blog.tsx
+++ src/components/App/index/Blog.tsx
@@ -1,13 +1,74 @@
-import ActionModule from '~/UI/ActionModule'
+'use client'
+
+import TelegramLogo from '$/index/telegram.svg'
+import TelegramInvertLogo from '$/index/telegram-invert.svg'
+
+import {useRef} from 'react'
+
+import Image from 'next/image'
+import Link from 'next/link'
+import {H2, H3, H6} from '~/UI/Typography'
+import HoverText from '~/UI/HoverText'
+
+const FaqData = {
+  caption: 'Блог',
+  title: 'Подпишитесь на наш телеграм-канал «Санкционный дозор»',
+  link: 'https://t.me/sanctionsexplained',
+  actionText: 'Telegram',
+  actionButtonText: 'Подписаться на канал',
+}
 
 export default function Blog() {
+  const linkRef = useRef<HTMLAnchorElement>(null)
+
   return (
-    <ActionModule
-      caption="Блог"
-      title="Подпишитесь на наш телеграм-канал «Санкционный дозор»"
-      link="https://t.me/sanctionsexplained"
-      actionText="Telegram" // big square button
-      actionButtonText="Подписаться на канал" // blue button
-    />
+    <section id="blog" data-section="blog-index">
+      <div data-section="desktop-blog-index" className="sm:hidden">
+        <div className="w-full h-8 bg-blue"></div>
+
+        <div className="grid grid-cols-4">
+          <div className="self-end col-span-2 p-8 space-y-3">
+            <H6>{FaqData.caption}</H6>
+            <H2 className="leading-[1.2]">{FaqData.title}</H2>
+          </div>
+
+          <div></div>
+
+          <Link href={FaqData.link} className="flex flex-col justify-between gap-20 p-8 xl:gap-12 group border-l border-gray" target="_blank">
+            <Image className="w-12 xl:w-10 group-hover:scale-[1.07] duration-300" src={TelegramLogo} alt="Телеграм канал Санкционный дозор" />
+
+            <H3 className="leading-[1.1] border-b-2 border-transparent group-hover:border-foreground w-fit duration-300">{FaqData.actionText}</H3>
+          </Link>
+        </div>
+
+        <Link ref={linkRef} href={FaqData.link} className="block pt-5 pb-3.5 text-center text-white bg-blue">
+          <HoverText triggerRef={linkRef}>
+            <H3 className="mx-auto leading-tight w-fit">{FaqData.actionButtonText}</H3>
+          </HoverText>
+        </Link>
+      </div>
+
+      <div data-section="mobile-blog-index" className="hidden sm:block">
+        <div className="w-full h-6 bg-blue"></div>
+
+        <div className="px-3 py-8 pb-12 space-y-8 border-b border-gray">
+          <div className="space-y-2">
+            <H6>{FaqData.caption}</H6>
+            <H2 className="!leading-[1.2] pr-4">{FaqData.title}</H2>
+          </div>
+
+          <div className="space-y-2">
+            <Link href={FaqData.link} className="flex items-center justify-center gap-2 py-3.5 text-white bg-blue" target="_blank">
+              <Image className="w-7" src={TelegramInvertLogo} alt="Телеграм канал Санкционный дозор" />
+              <H3 className="sm:text-lg">sanctionsexplained</H3>
+            </Link>
+
+            <Link href={FaqData.link} className="flex items-center justify-center gap-2 py-3.5 text-white bg-blue">
+              <H3 className="sm:text-lg">{FaqData.actionButtonText}</H3>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }

90eeae8a97d3cd4302edef824cbc3d16209ae71a
maxim <bozzhik@ya.ru>
2025-04-15 21:54:19 +0300

    set `dynamic` behavior for website `layout`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ import Notification from '~/Global/Notification'
 import Loader from '~/Global/Loader'
 import YandexMetrika from '~/Global/Analytics'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 86400
+
 export default function RootLayout({
   children,
 }: Readonly<{
